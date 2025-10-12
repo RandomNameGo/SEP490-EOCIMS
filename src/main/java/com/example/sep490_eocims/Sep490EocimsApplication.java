66f5914de338150503b9d12e7c04bd9e93182895
Admin <khangdoan20122004@gmail.com>
2025-10-12 21:49:47 +0700

    Initial commit

---
+++ src/main/java/com/example/sep490_eocims/Sep490EocimsApplication.java
@@ -0,0 +1,13 @@
+package com.example.sep490_eocims;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sep490EocimsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sep490EocimsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sep490_eocims/Sep490EocimsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sep490_eocims;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sep490EocimsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
